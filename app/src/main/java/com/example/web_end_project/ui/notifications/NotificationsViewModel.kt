480791804bc82ecd271a02189a5b46430ef31f07
patryjas <patrykjas08@gmail.com>
2024-03-10 09:25:00 +0100

    Android Init

---
+++ app/src/main/java/com/example/web_end_project/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.web_end_project.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
